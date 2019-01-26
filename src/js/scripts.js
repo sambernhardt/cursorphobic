3a6323be955f829f51a7bdd674613f3ee8e196bc
Samuel Bernhardt <sdbernhardt@gmail.com>
2019-01-26 12:30:32 -0500

    Initial commit

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,9 +1,72 @@
-(function ($, window, document, undefined) {
 
-  'use strict';
+var margin = 50;
+var proximity = 75;
 
-  $(function () {
-    // FastShell
-  });
+var button = document.querySelector(".button");
+var buttonWidth = button.offsetWidth;
+var buttonHeight = button.offsetHeight;
 
-})(jQuery, window, document);
+var xPos = document.querySelector("#xpos");
+var yPos = document.querySelector("#ypos");
+
+function positionButton(mouseX, mouseY) {
+  xPos.innerHTML = 'X: ' + mouseX;
+  yPos.innerHTML = 'Y: ' + mouseY;
+
+  var ww = window.innerWidth;
+  var wh = window.innerHeight;
+
+  var t = button.offsetTop;
+  var r = button.offsetLeft + button.offsetWidth;
+  var b = button.offsetTop + button.offsetHeight;
+  var l = button.offsetLeft;
+  var xc = button.offsetLeft + (button.offsetWidth / 2);
+  var yc = button.offsetTop + (button.offsetHeight / 2);
+
+  var dx = 0;
+  var dy = 0;
+  console.log(xc, yc);
+
+  // if the mouse is within 200 pixels of the outerwidth
+  if (mouseX > l - proximity || mouseX < r + proximity) {
+    // if the mouse is within 200 pixels of the outer height
+    if (mouseY < t - proximity || mouseY < b + proximity) {
+
+      if (mouseX + margin > l && mouseX < xc) {
+        dx = mouseX - ((ww/2) - (buttonWidth / 2)) + margin;
+      }
+      if (mouseX - margin < r && mouseX > xc) {
+        dx = mouseX - ((ww/2) + (buttonWidth / 2)) - margin;
+      }
+
+      if (mouseY + margin > t && mouseY < yc) {
+        dy = mouseY - ((wh/2) - (buttonWidth / 2)) + margin;
+      }
+      if (mouseY - margin < b && mouseY > yc) {
+        dy = mouseY - ((wh/2) + (buttonWidth / 2)) - margin;
+      }
+
+    }
+  }
+
+
+  var newX = dx;
+  var newY = dy;
+
+  button.style.transform = "translate("+newX+"px, "+newY+"px)";
+  // button.style.transform = "translateX("+newX+"px)";
+}
+
+
+document.addEventListener("mousemove", function(e) {
+  var ww = window.innerWidth;
+  var wh = window.innerHeight;
+
+  positionButton(e.pageX, e.pageY);
+  // var xmapped = scale(e.pageX, 0, ww, -100, 100);
+  // var ymapped = scale(e.pageY, 0, wh, 100, -100);
+
+
+})
+
+// console.log(button.offsetTop);
