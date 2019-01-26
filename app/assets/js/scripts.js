04740e17f3696fb178dd187297d02ce9c25a76d3
Samuel Bernhardt <sdbernhardt@gmail.com>
2019-01-26 13:56:51 -0500

    Finished objectified

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -6,70 +6,65 @@
  * @version 1.0.5
  * Copyright 2019. MIT licensed.
  */
-var margin = 0;
-var proximity = 0;
-
-var el = document.querySelector(".button");
-el.style.transition = "transform .1s";
-var main = document.querySelector(".main");
-var elPosition = getPosition(el);
-var elWidth = el.offsetWidth;
-var elHeight = el.offsetHeight;
-
 var xPos = document.querySelector("#xpos");
 var yPos = document.querySelector("#ypos");
 
 function cursorphobic(selector, config) {
-  margin = config.margin;
-  proximity = config.proximity;
+  var obj = {};
 
+  console.log(selector);
+  obj.margin = config.margin;
+  obj.proximity = config.proximity;
+
+  obj.el = document.querySelector(selector);
+  obj.el.style.transition = "transform .1s";
+  obj.elPosition = getPosition(obj.el);
+  obj.elWidth = obj.el.offsetWidth;
+  obj.elHeight = obj.el.offsetHeight;
+
+  obj.moveButton = function(mouseX, mouseY) {
+    xPos.innerHTML = 'X: ' + mouseX;
+    yPos.innerHTML = 'Y: ' + mouseY;
+
+    var t = obj.elPosition.y;
+    var r = obj.elPosition.x + obj.elWidth;
+    var b = obj.elPosition.y + obj.elHeight;
+    var l = obj.elPosition.x;
+    var xc = obj.elPosition.x + (obj.elWidth / 2);
+    var yc = obj.elPosition.y + (obj.elHeight / 2);
+
+    var dx = 0;
+    var dy = 0;
+
+    // if the mouse is within 200 pixels of the outerwidth
+    if (mouseY > t - obj.proximity && mouseY < b + obj.proximity) {
+
+        if (mouseX + obj.margin > l && mouseX < xc) {
+          dx = mouseX - l + obj.margin;
+        }
+        if (mouseX - obj.margin < r && mouseX > xc) {
+          dx = mouseX - r - obj.margin;
+        }
+
+    }
+
+    if (mouseX > l - obj.proximity && mouseX < r + obj.proximity) {
+
+        if (mouseY + obj.margin > t && mouseY < yc) {
+          dy = mouseY - t + obj.margin;
+        }
+        if (mouseY - obj.margin < b && mouseY > yc) {
+          dy = mouseY - b - obj.margin;
+        }
+
+    }
+
+    obj.el.style.transform = "translate("+dx+"px, "+dy+"px)";
+  }
 
   document.addEventListener("mousemove", function(e) {
-    moveButton(e.clientX, e.clientY);
+    obj.moveButton(e.clientX, e.clientY);
   })
-}
-
-function moveButton(mouseX, mouseY) {
-  xPos.innerHTML = 'X: ' + mouseX;
-  yPos.innerHTML = 'Y: ' + mouseY;
-
-  var ww = main.outerWidth;
-  var wh = main.outerHeight;
-
-  var t = elPosition.y;
-  var r = elPosition.x + elWidth;
-  var b = elPosition.y + elHeight;
-  var l = elPosition.x;
-  var xc = elPosition.x + (elWidth / 2);
-  var yc = elPosition.y + (elHeight / 2);
-
-  var dx = 0;
-  var dy = 0;
-
-  // if the mouse is within 200 pixels of the outerwidth
-  if (mouseY > t - proximity && mouseY < b + proximity) {
-
-      if (mouseX + margin > l && mouseX < xc) {
-        dx = mouseX - l + margin;
-      }
-      if (mouseX - margin < r && mouseX > xc) {
-        dx = mouseX - r - margin;
-      }
-
-  }
-
-  if (mouseX > l - proximity && mouseX < r + proximity) {
-
-      if (mouseY + margin > t && mouseY < yc) {
-        dy = mouseY - t + margin;
-      }
-      if (mouseY - margin < b && mouseY > yc) {
-        dy = mouseY - b - margin;
-      }
-
-  }
-
-  el.style.transform = "translate("+dx+"px, "+dy+"px)";
 }
 
 // Shawn Whinnery stack overflow
