435a36e42c2503acb3fefe95e08a8e0dcb22cb2e
Samuel Bernhardt <sdbernhardt@gmail.com>
2019-01-27 18:21:32 -0500

    Vertically centered content and added demo

--- app/assets/js/scripts.js
+++ app/assets/js/scripts.js
@@ -10,24 +10,37 @@ var xPos = document.querySelector("#xpos");
 var yPos = document.querySelector("#ypos");
 
 function cursorphobic(selector, config) {
+  // setup
   var obj = {};
-  obj.margin = 0;
+  // obj.margin = 0;
   obj.proximity = 0;
   obj.animationDelay = 0.1;
 
-  if (config.margin != undefined) obj.margin = config.margin;
+  // obj.label = document.createElement("p");
+  // obj.label.style.position = 'absolute';
+  // document.querySelector('body').appendChild(obj.label);
+
+  // if (config.margin != undefined) obj.margin = config.margin;
   if (config.proximity != undefined) obj.proximity = config.proximity;
   if (config.animationDelay != undefined) obj.animationDelay = config.animationDelay;
+  obj.margin = obj.proximity;
 
   obj.el = document.querySelector(selector);
   obj.el.style.transition = obj.el.style.transition + " transform " + obj.animationDelay + "s";
   obj.elPosition = getPosition(obj.el);
   obj.elWidth = obj.el.offsetWidth;
   obj.elHeight = obj.el.offsetHeight;
+  obj.el.onclick = function() {
+    alert("You did it!")
+  }
 
   obj.moveButton = function(mouseX, mouseY) {
     xPos.innerHTML = 'X: ' + mouseX;
     yPos.innerHTML = 'Y: ' + mouseY;
+
+    // obj.label.style.left = mouseX + 'px';
+    // obj.label.style.top = mouseY + 'px';
+    // obj.label.innerHTML = "You gotta be quicker than that.";
 
     var t = obj.elPosition.y;
     var r = obj.elPosition.x + obj.elWidth;
@@ -66,7 +79,9 @@ function cursorphobic(selector, config) {
   }
 
   document.addEventListener("mousemove", function(e) {
-    obj.moveButton(e.clientX, e.clientY);
+    if (!config.noMove) {
+      obj.moveButton(e.clientX, e.clientY);
+    }
   })
 }
 
